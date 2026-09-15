c20be28dee92438450736d168cc5a50ab7ee7b33
rodrigsapps <contadiscord2209k@gmail.com>
2026-09-15 18:08:49 -0300

    SpyTeste: novo webhook

--- KoalaSpyTeste.lua
+++ KoalaSpyTeste.lua
@@ -12,7 +12,7 @@ aviso("request/http_request: " .. (req and "SIM" or "NAO"))
 
 if req then
     local ok, res = pcall(function()
-        return req({Url="https://discord.com/api/webhooks/1546164688994177112/FVxL0qlwRYpI6ITaoTbK0qqchk2cxCrxB_r_5DDDV_HGEAzmYB5lDBwCyqtLI2GYiZ52",
+        return req({Url="https://discord.com/api/webhooks/1549385323044151296/7T_ZOMoz2BOd5RdiZo4peuYEsXxIhbReVP9_gKIIR_nIw6yOvBfAxBOppnldMP3AHJng",
             Method="POST", Headers={["Content-Type"]="application/json"},
             Body=game:GetService("HttpService"):JSONEncode({content="**[KoalaSpy] TESTE direto do executor — webhook OK**"})})
     end)
